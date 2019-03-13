719a8e77e1257e92fa94abb96a8582d155907768
Sahil Mehta <sahilmehta17@gmail.com>
2019-03-13 16:57:24 -0400

    Create initial process for initializing a puzzle

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -4,17 +4,29 @@ import java.util.List;
 class Puzzle {
 
     // ==================== Fields ====================
+    // Side length of the puzzle (e.g. 4)
     private int dimension;
+
+    // Maps location to the cage that owns the location
+    private Cage[][] cageMap;
 
     // ==================== Constructors ====================
     // cageSquareLocations is {{x1, y1}, {x2, y2}, ...}
     public Puzzle(
         int dimension,
-        List<int[][]> cageSquareLocations,
+        int[][][] cageSquareLocations,
         List<Integer> cageTargets,
         List<Operation> cageOperations
     ) {
         this.dimension = dimension;
+        cageMap = new Cage[dimension][dimension];
     }
+
+    private void initializeCageMap(int[][][] cageSquareLocations) {
+        
+    }
+
+    // ==================== Utilities ====================
+
 
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,41 @@
+import java.util.List;
+import java.util.ArrayList;
 
 class main {
     public static void main(String args[]) {
         System.out.println("Hello, world!");
-        Square s = new Square(1, 2);
+
+        int[][][] testPuzzle = {
+            {{0, 0}, {0, 1}},
+            {{0, 2}, {0, 3}},
+            {{1, 0}, {1, 1}},
+            {{1, 2}, {2, 2}, {3, 2}},
+            {{1, 3}},
+            {{2, 0}, {2, 1}, {3, 1}},
+            {{2, 3}, {3, 3}},
+            {{0, 0}}
+        };
+
+        List<Integer> testTargets = new ArrayList<Integer>();
+        testTargets.add(2);
+        testTargets.add(2);
+        testTargets.add(3);
+        testTargets.add(9);
+        testTargets.add(4);
+        testTargets.add(9);
+        testTargets.add(2);
+        testTargets.add(4);
+
+        List<Operation> testOps = new ArrayList<Operation>();
+        testOps.add(Operation.SUB);
+        testOps.add(Operation.SUB);
+        testOps.add(Operation.ADD);
+        testOps.add(Operation.ADD);
+        testOps.add(null);
+        testOps.add(Operation.MULT);
+        testOps.add(Operation.DIV);
+        testOps.add(null);
+
+        Puzzle p = new Puzzle(4, testPuzzle, testTargets, testOps);
     }
 }
