fde2af1fb9c02fc3af2652c341d469c639c48bdc
Sahil Mehta <sahilmehta17@gmail.com>
2019-03-13 16:01:32 -0400

    Add legalOptions and value fields to Square class

--- src/Square.java
+++ src/Square.java
@@ -1,8 +1,35 @@
 
+import java.util.Set;
+import java.util.HashSet;
+
 class Square {
 
+    // Set of legal options remaining for this square
+    private Set<Integer> legalOptions;
+    private int value = 0;
+
     public Square() {
-        System.out.println("New square was created.");
+        legalOptions = new HashSet<Integer>();
+    }
+
+    public Set<Integer> getLegalOptions() {
+        return legalOptions;
+    }
+
+    public void addLegalOption(int optionToAdd) {
+        legalOptions.add(optionToAdd);
+    }
+
+    public void removeLegalOption(int optionToRemove) {
+        legalOptions.remove(optionToRemove);
+    }
+
+    public void setValue(int newValue) {
+        value = newValue;
+    }
+
+    public void getValue() {
+        return value;
     }
 
 }
